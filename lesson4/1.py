45a3ffd450e0ea742c96a8ae9f4e5825d5098ff3
makul_oleg <makul_oleg@mail.ru>
2021-12-08 13:14:14 +0500

    Add files via upload

--- lesson4/1.py
+++ lesson4/1.py
@@ -1,7 +1,9 @@
-def simple_calc():
-    x = float(input('Введите количество отработанных часов : '))
-    y = float(input('Введите суммы оплаты труда за 1 час : '))
-    c = float(input('Укажите размер премии - '))
-    pay = (x * y) + c
-    return pay
-print(f'Размер заработной платы составил: {simple_calc() }')
+
+def simple_calc(hours_worked, sum_pay, awards):
+    return (hours_worked * sum_pay) + awards
+
+
+hours_worked = float(input('Введите количество отработанных часов : '))
+sum_pay = float(input('Введите суммы оплаты труда за 1 час : '))
+awards = float(input('Укажите размер премии - '))
+print(f'Размер заработной платы составил: {simple_calc(hours_worked, sum_pay, awards) }')
