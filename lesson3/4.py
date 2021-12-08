7655ba29d963e3fe06230d7ef47f4c3592cfe783
makul_oleg <makul_oleg@mail.ru>
2021-12-08 09:25:53 +0500

    Add files via upload

--- lesson3/4.py
+++
@@ -1,7 +0,0 @@
-def my_func(x, y):
-    return print(x**y)
-my_func(
-    int(input('Число Х ')),
-    int(input('в степень Y '))
-
-)
--- lesson3/6.py
+++
@@ -1,7 +0,0 @@
-def int_func(text):
-    return text.title()
-
-out_text = []
-for word in input('Введите строку чисел, разделенных пробелом').split(' '):
-    out_text.append(int_func(word))
-print(" ".join(out_text))
