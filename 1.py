f9ee4b7347dddf7515bed68ae8c730fc2467add1
makul_oleg <makul_oleg@mail.ru>
2021-12-23 14:53:57 +0500

    Add files via upload

--- 1.py
+++
@@ -1,8 +0,0 @@
-file = open('test.txt', 'w')
-text = input('Введите текст \n')
-while text:
-    file.writelines(f'{text} \n')
-    text = input('Введите текст \n')
-    if not text:
-        break
-file.close()
